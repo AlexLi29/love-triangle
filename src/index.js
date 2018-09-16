db91f5195e41dedb1e7149eacb9d56950f421cf0
AlexLi29 <Alexandra_litosh@mail.ru>
2018-09-16 16:51:22 +0300

    love-triangle works

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var k, c, b, n, i;
+  c = 0;
+  i = 0;
+      for (n = 0; n < preferences.length; n++){
+        i = 0;
+        b = n;
+        k = preferences [n]; 
+          while (k != n+1 && i < 4){
+          k = preferences [b];     
+          b = k-1;
+          i ++;
+               };
+                  if (i == 3){
+                  c ++;
+                  };
+      };
+  return c/3;
 };
